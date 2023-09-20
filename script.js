74c8f10a57d8b79bb9e907d1d7605091ac62dba3
Yohannes Mulu <dev.yohannes@gmail.com>
2023-09-20 20:22:03 +0300

    adding score counter and adding counter to the retry button

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ const checkButton = document.querySelector("#check-button");
 const retryButton = document.querySelector("#retry");
 const resultText = document.querySelector("#result-text");
 const tryCounter = document.querySelector("#try-counter");
+const score = document.querySelector("#score");
 
-//
-// functions
-//
+// --------functions --------
 const checking = () => {
   // setting up random number
   const roundedRandomNumber = Math.round(Math.random() * 5);
@@ -30,13 +29,22 @@ const checking = () => {
       container.classList.add("error");
     }
 
-    // console.log("user number -- ", numberedRandomNumberInput);
-    // console.log("random number -- ", roundedRandomNumber);
+    console.log("user number -- ", numberedRandomNumberInput);
+    console.log("random number -- ", roundedRandomNumber);
+  }
+};
+
+scoreCounter = 1;
+
+const scoreValue = () => {
+  // console.log(container.classList);
+  if (container.className.includes("correct")) {
+    // console.log("correct value");
+    score.innerHTML = `You got: ${scoreCounter++}/5`;
   }
 };
 
 counter = 1;
-
 const counterFunction = () => {
   if (counter <= 5) {
     tryCounter.innerHTML = `Tries: ${counter++}/5`;
@@ -47,15 +55,16 @@ const counterFunction = () => {
     resultText.innerHTML = "";
     checkButton.classList.add("display-none-check-button");
     retryButton.classList.remove("display-none-retry-button");
+
+    scoreValue();
   }
 };
 
-//
-// event listeners
-//
+// -------- event listeners --------
 checkButton.addEventListener("click", function () {
   checking();
   counterFunction();
+  // scoreValue();
   userNumberInput.value = "";
   // userNumberInput.autofocus = true;
 });
@@ -64,6 +73,7 @@ document.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     checking();
     counterFunction();
+    // scoreValue();
     userNumberInput.value = "";
   }
 });
@@ -76,4 +86,5 @@ retryButton.addEventListener("click", function () {
   userNumberInput.disabled = false;
   checkButton.classList.remove("display-none-check-button");
   retryButton.classList.add("display-none-retry-button");
+  counter = 1;
 });
